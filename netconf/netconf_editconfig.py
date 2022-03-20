969055a1fe1731c72783544339a9392b1b36b501
malu <chikamaluj@gmail.com>
2022-03-20 02:41:30 +0100

    routing_info

--- netconf/netconf_editconfig.py
+++ netconf/netconf_editconfig.py
@@ -3,7 +3,7 @@ import xmltodict
 from pprint import pprint
 from device_info import CSR1000V
 
-with open('edit_filter.xml', 'r') as f:
+with open('edit_filter.xml') as f:
     edit_filter = f.read()
     edit_int = edit_filter.format(interface_name='Loopback69', interface_des='malu was here')
     m = manager.connect(host=CSR1000V['host'],port=CSR1000V['port'],username=CSR1000V['username'],password=CSR1000V['password'],hostkey_verify=False)
