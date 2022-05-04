9159d5cddc90b3e4333fd094a4331ef19491acec
malu <chikamaluj@gmail.com>
2022-05-04 21:28:38 +0100

    sdwan

--- dna/dna_sdk.py
+++ dna/dna_sdk.py
@@ -5,9 +5,9 @@ from pprint import pprint
 #establish connection to controller
 api = DNACenterAPI(username='devnetuser',password='Cisco123!',base_url='https://sandboxdnac.cisco.com',version='1.2.10',verify=False)
 #get list of all devices
-devices = api.devices.get_device_list()
+devices_lst = api.devices.get_device_list()
 #get deviceid of a particular device
-deviceid = devices['response'][0]['id']
+deviceid = devices_lst['response'][0]['id']
 #get the all interface info of all the device for the above deviceid
 ints = api.devices.get_interface_info_by_id(device_id=deviceid)
 pprint(ints)
---
+++ sdwan/get_device.py
@@ -0,0 +1,34 @@
+import requests
+import json 
+from pprint import pprint
+
+url = "https://10.10.20.90:443/j_security_check"
+
+login = {
+    'j_username':'admin',
+    'j_password':'C1sco12345'
+}
+#maintain SD-WAN session 
+session = requests.session()
+
+response = session.post(url,data=login,verify=False)
+#verify login
+if not response.ok or response.text:
+    print('login fail')
+    import sys
+    sys.exit(1)
+else:
+    print('login worked')
+#get list of devices
+url = url = "https://10.10.20.90:443/dataservice/device"
+
+device_resp = session.get(url,verify=False).json()['data']
+# pprint(device_resp)
+
+for device in device_resp:
+    pprint(device)
+    # print(f"HOSTNAME:{device['host-name']}")
+    # print(f"IP_ADD{device['local-system-ip']}")
+    # print(f"MODEL:{device['device-model']}")
+    # print(f"STATE:{device['status']}")
+    # print(' ')
